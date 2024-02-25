18b7d37a66e3bc43c2da58a876ddeef703ce695b
kato0116 <j12shun@gmail.com>
2024-02-25 19:21:12 +0900

    [add]ddpm_mnist対応

---
+++ DDPM_mnist/main.py
@@ -0,0 +1,71 @@
+import torch
+import matplotlib.pyplot as plt
+import torchvision
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from torch.optim import Adam
+import torch.nn.functional as F
+
+from tqdm import tqdm
+
+from diffuser import Diffuser,UNet
+from show_imgs import show_images
+
+if __name__ == "__main__":
+    img_size = 28
+    batch_size = 128
+    num_timesteps = 1000
+    epochs = 10
+    lr = 1e-3
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    preprocess = transforms.ToTensor()
+    dataset = torchvision.datasets.MNIST(root='./data', download=True, transform=preprocess)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    diffuser = Diffuser(num_timesteps, device=device)
+    model = UNet()
+    model.to(device)
+    optimizer = Adam(model.parameters(), lr=lr)
+
+    losses = []
+    for epoch in range(epochs):
+        loss_sum = 0.0
+        cnt = 0
+
+        # generate samples every epoch ===================
+        # images = diffuser.sample(model)
+        # show_images(images)
+        # ================================================
+
+        for images, labels in tqdm(dataloader):
+            optimizer.zero_grad()
+            x = images.to(device)
+            t = torch.randint(1, num_timesteps+1, (len(x),), device=device)
+
+            x_noisy, noise = diffuser.add_noise(x, t)
+            noise_pred = model(x_noisy, t)
+            loss = F.mse_loss(noise, noise_pred)
+
+            loss.backward()
+            optimizer.step()
+
+            loss_sum += loss.item()
+            cnt += 1
+
+        images = diffuser.sample(model)
+        show_images(images,epoch+1)
+
+        loss_avg = loss_sum / cnt
+        losses.append(loss_avg)
+        print(f'Epoch {epoch} | Loss: {loss_avg}')
+
+    # plot losses
+    plt.plot(losses)
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.savefig("/root/log/loss.png")
+
+    # generate samples
+    images = diffuser.sample(model)
+    show_images(images,"pred")
