788aeeec65c1ef9d4591e56d637d8352f9e28361
kato0116 <j12shun@gmail.com>
2024-02-28 00:55:42 +0900

    [update]ログ記録

--- DDPM_mnist_condition/main.py
+++ DDPM_mnist_condition/main.py
@@ -10,15 +10,29 @@ from tqdm import tqdm
 
 from diffuser import Diffuser,UNet
 from show_imgs import show_images
+import pandas as pd
+import wandb
+
 
 if __name__ == "__main__":
     img_size = 28
     batch_size = 128
     num_timesteps = 1000
-    epochs = 10
+    epochs = 100
     lr = 1e-3
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print(torch.cuda.is_available())
+
+    # wandbのセットアップ
+    wandb.init(
+        project="ddpm_cond_mnist",
+        config={
+        "learning_rate": 1e-3,
+        "architecture": "DDPM_cond",
+        "dataset": "MNIST",
+        "epochs": 100,
+        }
+    )
 
     preprocess = transforms.ToTensor()
     dataset = torchvision.datasets.MNIST(root='/root/data', download=True, transform=preprocess)
@@ -61,13 +75,23 @@ if __name__ == "__main__":
         loss_avg = loss_sum / cnt
         losses.append(loss_avg)
         print(f'Epoch {epoch} | Loss: {loss_avg}')
-
-    # plot losses
-    plt.plot(losses)
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.savefig("/root/log/loss.png")
+        
+        # wandbにlogの送信
+        wandb.log({"loss":loss_avg})
+        
+    log = {"epoch":range(epochs), "loss":losses}
+    df_log = pd.DataFrame(log)
+    df_log.to_csv('/root/nishilab/DDPM_mnist_condition/log/loss.csv',index=False)
+        
+    model_path = '/root/nishilab/DDPM_mnist_condition/weights/unet_cond_mnist.pth'
+    torch.save(model.state_dict(), model_path)
 
     # generate samples
     images,labels = diffuser.sample(model)
     show_images(images,labels,"pred")
+    
+    wandb.alert(
+        title = "実行終了",
+        text  = "アーキテクチャ: DDPM, データセット: MNIST"
+    )
+    wandb.finish()
--- DDPM_mnist_condition/show_imgs.py
+++ DDPM_mnist_condition/show_imgs.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
+
 def show_images(images, labels, epoch, rows=2, cols=10):
     fig = plt.figure(figsize=(cols, rows))
     i = 0
     for r in range(rows):
         for c in range(cols):
             ax = fig.add_subplot(rows, cols, i + 1)
-            ax.set_xlabel(labels[i].item())
             plt.imshow(images[i], cmap='gray')
-            plt.axis('off')
+            ax.set_xlabel(labels[i].item())
+            ax.get_xaxis().set_ticks([])
+            ax.get_yaxis().set_ticks([])
             i += 1
-    plt.savefig(f"/root/log/epoch_{epoch}.png")
+    plt.tight_layout()
+    plt.savefig(f"/root/nishilab/DDPM_mnist_condition/log/epoch_{epoch}.png")
